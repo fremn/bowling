1fda25e23d6db15a26b0c1caa7f6492a61f7bca9
Fremen DeRuvo <fremenderuvo@gmail.com>
2013-09-12 15:49:19 -0500

    refactors score method by decompositing spare logic to spare? and spare_score methods

--- bowling_game.rb
+++ bowling_game.rb
@@ -3,6 +3,8 @@ class BowlingGame
   
   def initialize
     @rolls = []
+    @total_score  = 0
+    @current_roll = 0
   end
   # Record a roll in the game.
   # pins = The Integer number of pins knocked down in this roll. 
@@ -12,25 +14,46 @@ class BowlingGame
   
   # sums all rolls
   def score
-    total_score = 0
-    current_roll = 0
+    while @current_roll < @rolls.size - 1
+      init_roll
 
-    while current_roll < @rolls.size - 1
-      roll = @rolls[current_roll]
-      next_roll = @rolls[current_roll + 1 ]
-
-      if roll == 10
-        total_score += 10 + next_roll + @rolls[current_roll +2]
-        current_roll +=1
-      elsif roll + next_roll == 10
-        total_score += 10 + @rolls[current_roll + 2]
-        current_roll += 2
+      if strike?
+        score_strike
+      elsif spare?
+        score_spare 
       else
-        total_score += roll + next_roll
-        current_roll += 2
+        @total_score += @roll + @next_roll
+        @current_roll += 2
       end      
     end
-    
-    return total_score
+
+    return @total_score
+  end
+
+  private #methods for this class only
+  # Returns true if the current roll is a strike, false otherwise.
+  
+  def init_roll
+    @roll       = @rolls[@current_roll]
+    @next_roll  = @rolls[@current_roll + 1] 
+  end
+  
+  def spare?
+    @roll + @next_roll == 10
+  end
+
+  def score_spare 
+    @total_score += 10 + @rolls[@current_roll + 2]
+    @current_roll += 2
+  end
+
+       
+  def strike?
+    @roll == 10
+  end
+
+  def score_strike
+    @total_score += 10 + @next_roll + @rolls[@current_roll + 2]
+    @current_roll += 1
   end
 end
