b29cae74c1b1f45fb95de12ff13be3345017f924
Fremen DeRuvo <fremenderuvo@gmail.com>
2013-09-12 13:55:06 -0500

    adds and implements test to check that if a bowler got all gutters there score is 0

--- bowling_game.rb
+++ bowling_game.rb
@@ -1,7 +1,17 @@
 class BowlingGame
   attr_accessor :rolls
+  
   def initialize
     @rolls = []
   end
-
+  # Record a roll in the game.
+  # pins = The Integer number of pins knocked down in this roll.
+  #
+  # Returns Nothing
+  def roll(pins)
+  end
+  
+  def score
+    0
+  end
 end
